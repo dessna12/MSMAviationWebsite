9e3da2f9e270591ccd592746dc9a0ffe9c6a9c1e
dessna12 <92819503+dessna12@users.noreply.github.com>
2023-01-31 18:29:26 +0100

    Animation page index + Footer + animation nav

---
+++ footer.php
@@ -0,0 +1,4 @@
+<?php
+echo '<p>MSM AVIATION, 1 impasse du Cluzel, 69200 Venissieux FRANCE |  <span a="#">Disclaimer</span>  | <span a="#">General Terms of sale</span>  | All rights reserved ©2023 MSM Aviation 
+</p>';
+?>
