6f7dd0083dc14983d7f3e050f43471b90c0403d3
dessna12 <92819503+dessna12@users.noreply.github.com>
2023-02-20 17:06:23 +0100

    Our capabilities section + contact version mobile

--- animations.js
+++ animations.js
@@ -1,4 +1,4 @@
-
+gsap.registerPlugin(ScrollTrigger);
 
 //GET ACCESS DOM
 const elementMenu = document.querySelectorAll("a.element-menu");
@@ -24,3 +24,91 @@ checkboxMenu.addEventListener('change', ($event) => {
         burgerMenu.textContent = 'menu';
     }
 });
+
+
+//Fais apparaitre les éléments au fur et à mesure on scroll
+window.addEventListener("scroll", appear);
+
+
+
+//FUNCTIONS to reveal elements on scroll
+function appear() {
+    var appears = document.querySelectorAll(".appear, .reveal, .rotate");
+  
+    for(let i = 0; i < appears.length; i++) {
+      var windowHeight = window.innerHeight;
+      var elementTop = appears[i].getBoundingClientRect().top;
+      var elementVisible = 100;
+  
+      if (elementTop < windowHeight - elementVisible) {
+        appears[i].classList.add("active");
+      }else{
+        appears[i].classList.remove("active")
+      }
+    }
+  }
+
+
+
+//TRANSITIONS DE PAGE
+function pageTransition() {
+    var tl = gsap.timeline();
+    tl.to(".loading-screen", {
+        duration: 0.5,
+        width: "100%",
+        top: "0%",
+        ease: "Expo.easeInOut",
+    });
+
+    tl.to(".loading-screen", {
+        duration: 1,
+        width: "100%",
+        top: "100%",
+        ease: "Expo.easeInOut",
+        delay: 0.3,
+    });
+    tl.set(".loading-screen", { left: "-100%" });
+}
+
+function contentAnimation() {
+    var tl = gsap.timeline();
+    tl.from(".animate-this", { duration: 1, y: 30, opacity: 0, stagger: 0.4, delay: 0.2 });
+}
+
+
+$(function () {
+    barba.init({
+        sync: true,
+
+        transitions: [
+            {
+                async leave(data) {
+                    const done = this.async();
+
+                    pageTransition();
+                    await delay(500);
+                    done();
+                }
+
+                // async enter(data) {
+                //     contentAnimation();
+                // },
+
+                // async once(data) {
+                //     contentAnimation();
+                // },
+            },
+        ],
+    });
+});
+
+function delay(n) {
+    n = n || 2000;
+    return new Promise((done) => {
+        setTimeout(() => {
+            done();
+        }, n);
+    });
+}
+
+
